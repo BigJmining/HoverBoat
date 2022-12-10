f221d81b268a728f3b0b14c299e3df766290df10
Jason <jlohrey09@gmail.com>
2022-12-10 15:19:40 -0800

    added rock and boat sprites, rotations, and thrust

--- HoverBoat.py
+++ HoverBoat.py
@@ -12,31 +12,50 @@ and ship movement
 # import libraries needed
 import pygame
 from os.path import join
+import random
 
 import sprites 
 
 pygame.init()
 
-SIZE = SCREENWIDTH,SCREENHEIGHT= 600,300
+SIZE = SCREENWIDTH,SCREENHEIGHT= 1200,600
 FPS = 60
+
 
 window = pygame.display.set_mode((SIZE))
 pygame.display.set_caption("HOVERBOAT 2052")
 clock = pygame.time.Clock()
 
 opening_splashscreen = pygame.image.load(join('images','opening.png'))
-
+water_image = pygame.image.load(join('images','water.png'))
+water = pygame.transform.scale(water_image,(SIZE))
+opening = pygame.transform.scale(opening_splashscreen,(SIZE))
 
 HB = sprites.Hoverboat(-10,210,50,50)
+ROCKS = [
+    sprites.Rock(   50,  0, 50, 50), 
+    sprites.Rock( -350, 50, 50, 50), 
+    sprites.Rock( -650, 40, 50, 50), 
+    sprites.Rock( -150,100, 50, 50), 
+    sprites.Rock(    0,200, 50, 50) 
+    ]
+
 splashscreen = True
 def redrawGameWindow():
     window.fill((0,60,250))
-    if (splashscreen):
-        window.blit(opening_splashscreen,(0,0))
-
+    window.blit(water,(0,0))
+    
+    
     HB.draw(window)
 
+    for ROCK in ROCKS:
+        ROCK.draw(window)
+
+    if (splashscreen):
+        window.blit(opening,(0,0))
+
     pygame.display.update()
+
 
 run = True
 while run:
@@ -63,14 +82,14 @@ while run:
         HB.right = False
     elif keys[pygame.K_LEFT]:
         splashscreen = False
-        HB.angle += 5
+        HB.angle += 2
         HB.up = False
         HB.down = False
         HB.left = True
         HB.right = False
     elif keys[pygame.K_RIGHT]:
         splashscreen = False
-        HB.angle += -5
+        HB.angle-= 2
         HB.up = False
         HB.down = False
         HB.left = False
--- sprites.py
+++ sprites.py
@@ -1,8 +1,9 @@
 import pygame
 from os.path import join
+import math
 
-SIZE = SCREENWIDTH,SCREENHEIGHT= 600,300
 
+SIZE = SCREENWIDTH,SCREENHEIGHT= 1200,600
 
 class Hoverboat:
     def __init__(self,x,y,width,height):
@@ -10,11 +11,13 @@ class Hoverboat:
         self.y = y
         self.width = width
         self.height = height
+        self.hitbox = (self.x + 17, self.y +11, 32, 55)
         self.right = False
         self.left = False
         self.up = True
         self.down = False
         self.angle = 0
+        self.angle_change = 0
         self.thrust = 0
         self.vel_x = 1
         self.vel_y = 1
@@ -27,6 +30,17 @@ class Hoverboat:
         self.image_right_thrust = pygame.transform.rotate(self.image_up_thrust,-90)
         self.image_left = pygame.transform.flip(self.image_right,1,0)
         self.image_left_thrust = pygame.transform.flip(self.image_right_thrust,1,0)
+        self.rect = self.image_up.get_rect()
+        self.speed = 0
+
+    def addVectors(angle1, length1, angle2, length2):
+        x  = math.sin(angle1) * length1 + math.sin(angle2) * length2
+        y  = math.cos(angle1) * length1 + math.cos(angle2) * length2
+        
+        angle = 0.5 * math.pi - math.atan2(y, x)
+        length  = math.hypot(x, y)
+
+        return (angle, length)
 
     def move(self):
         if self.vel_x > 0:
@@ -63,22 +77,76 @@ class Hoverboat:
                 if self.y - self.thrust <= 0 - self.height:
                     self.vel_y = self.vel_y * -1
 
+
+
     def draw(self,window):
         self.move()
-        if (self.up):
-            window.blit(self.image_up,(self.x,self.y))
-            if (self.thrust):
-                window.blit(self.image_up_thrust,(self.x,self.y))
-        if (self.down):
-            window.blit(self.image_down,(self.x,self.y))
-            if (self.thrust):
-                window.blit(self.image_down_thrust,(self.x,self.y))
-        if (self.right):
-            window.blit(self.image_right,(self.x,self.y))
-            if (self.thrust):
-                window.blit(self.image_right_thrust,(self.x,self.y))
-        if (self.left):
-            window.blit(self.image_left,(self.x,self.y))
-            if (self.thrust):
-                window.blit(self.image_left_thrust,(self.x,self.y))
         
+
+        image = self.image_up
+        rotated_image = pygame.transform.rotate(image,+float(self.angle))
+        window.blit(rotated_image,(self.x,self.y))
+
+        self.hitbox = (self.x + 5, self.y +0, 80, 80)
+        pygame.draw.rect(window,(255,0,0),self.hitbox,2)
+        
+        
+class Rock:
+    def __init__(self,x,y,width,height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel_x = 1
+        self.vel_y = 1
+        self.max_vel = 2
+        self.thrust = 1
+        self.angle = 0
+        self.image = pygame.image.load(join('images','rock.png'))
+
+    def move(self):
+        if self.vel_x > 0:
+            if self.x + self.thrust < SCREENWIDTH - self.width:
+                self.x += self.thrust * self.vel_x
+                # print('l-r ',self.x,'-',self.vel_x)
+                if self.x +self.thrust >= SCREENWIDTH-self.width:
+                    if self.vel_x < self.max_vel:
+                        self.vel_x += 0.5
+                    elif self.vel_x == self.max_vel:
+                        self.vel_x = self.thrust
+                    self.vel_x = self.vel_x * -1
+        if self.vel_x < 0:
+            if self.x - self.thrust > 0-self.width:
+                self.x += self.thrust * self.vel_x
+                # print('r-l ',self.x,'-',self.vel_x)
+                if self.x - self.thrust <= 0 - self.width:
+                    self.vel_x = self.vel_x * -1
+
+        if self.vel_y > 0:
+            if self.y + self.thrust < SCREENHEIGHT - self.height:
+                self.y += self.thrust * self.vel_y
+                # print('l-r ',self.y,'-',self.vel_y)
+                if self.y +self.thrust >= SCREENHEIGHT-self.height:
+                    if self.vel_y < self.max_vel:
+                        self.vel_y += 0.5
+                    elif self.vel_y == self.max_vel:
+                        self.vel_y = 1
+                    self.vel_y = self.vel_y * -1
+        if self.vel_y < 0:
+            if self.y - self.thrust > 0-self.height:
+                self.y += self.thrust * self.vel_y
+                # print('r-l ',self.y,'-',self.vel_y)
+                if self.y - self.thrust <= 0 - self.height:
+                    self.vel_y = self.vel_y * -1
+
+    
+
+    def draw(self,window):
+        self.move()
+        image = self.image
+
+        rotated_image = pygame.transform.rotate(image,+float(self.angle))
+        window.blit(rotated_image,(self.x,self.y))
+
+        self.hitbox = (self.x + 5, self.y +0, 80, 80)
+        # pygame.draw.rect(window,(255,0,0),self.hitbox,2)
